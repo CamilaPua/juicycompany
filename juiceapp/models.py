ef813b9750918f2fdda01e68a99605f469b8de89
Camila Pua <camilapua7@gmail.com>
2023-11-03 16:24:24 -0500

    add app and models

--- juiceapp/models.py
+++ juiceapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Juice(models.Model):
@@ -8,12 +9,8 @@ class Juice(models.Model):
     def __str__(self) -> str:
         return self.name
 
-# class Client(models.Model):
-#     name = models.CharField(max_length=128, unique=True)
-#     email = models.EmailField()
-
 class Sale(models.Model):
-    # client = models.ForeignKey(Client)
+    client = models.ForeignKey(User)
     juice = models.ForeignKey(Juice)
     date = models.DateField(auto_now=True)
 
