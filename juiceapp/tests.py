22da901a3e1902d762108af9815aa9a10e069390
Ivan Vizcaino <ivizcain4@cuc.edu.co>
2023-11-07 15:00:40 -0500

    Updated urls, now empty name redirect to '/j../purchase'

--- juiceapp/tests.py
+++ juiceapp/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client
 from .models import User
 
 class PurchaseViewTest(TestCase):
+
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='password123')
         self.client = Client()
@@ -9,7 +10,11 @@ class PurchaseViewTest(TestCase):
 
     def test_purchase_view(self):
         response = self.client.get('/juiceapp/purchase/')
-
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'User ID: {}'.format(self.user.id))
         self.assertContains(response, 'User name: testuser')
+    
+    def test_purchase_view_logedout(self):
+        self.client.logout()
+        response = self.client.get('/juiceapp/purchase/')
+        self.assertEqual(response.status_code, 302)
--- juiceapp/urls.py
+++ juiceapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
 
     path('purchase/', views.purchase, name='purchase'),
+    path('', RedirectView.as_view(url='/juiceapp/purchase/')),
 
 ]
