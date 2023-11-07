f4d5cffe49c881b8d5aad6b9bac33b21df06220b
Ivan Vizcaino <ivizcain4@cuc.edu.co>
2023-11-07 00:47:24 -0500

    Updated view content request

--- juiceapp/tests.py
+++ juiceapp/tests.py
@@ -1,18 +1,15 @@
 from django.test import TestCase, Client
 from .models import User
 
-class MyModelTestCase(TestCase):
-    def test_create_user_and_print(self):
-        user = User.objects.create_user(username="testuser", password="password123")
-        print(f"User Name: {user.username}")
-        print(f"User ID: {user.id}")
-        self.assertEqual(user.username, "testuser")
-        self.assertGreater(user.id, 0)
+class PurchaseViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password123')
+        self.client = Client()
+        self.client.login(username='testuser', password='password123')
 
     def test_purchase_view(self):
-        client = Client()
-        client.login(username="testuser", password="password123")
-
-        response = client.get('/juiceapp/purchase/')
+        response = self.client.get('/juiceapp/purchase/')
 
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'User ID: {}'.format(self.user.id))
+        self.assertContains(response, 'User name: testuser') 
--- juiceapp/views.py
+++ juiceapp/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from .models import *
 
 def purchase(request):
+    user=request.user
     content={
-        'user_id': 0,
-        'user_name': "test"
+        'user_id': user.id,
+        'user_name': user.username
     }
     return render(request, 'purchase.html',content)
